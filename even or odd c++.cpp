0e0116f8d0bb6f528671b194a1e2da3a8fdb15ff
Prateek Reddy <113003361+PrateekReddy07@users.noreply.github.com>
2022-09-08 13:11:51 +0530

    Add files via upload

---
+++ even or odd c++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<< "Provide a number";
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<"even number";
+	}
+	else
+	{
+		cout<<"not even";
+	}
+	return 0;
+}
